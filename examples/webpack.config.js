3852b5cde8324c81d7aa3c22efa8322338e55238
caesor <liaozksysu@gmail.com>
2017-01-17 21:52:07 +0800

    update confilct

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const path = require('path'),
     webpack = require('webpack');
-=======
-const path = require('path');
->>>>>>> master
 
 module.exports = {
     entry: './index.js',
@@ -22,29 +18,22 @@ module.exports = {
             loaders: ['style', 'css', 'less']
         },
         {
-<<<<<<< HEAD
             test: /\.(png|jpg|gif|woff|woff2)$/,
             loader: 'url-loader?limit=8192'
         },
         {
-=======
->>>>>>> master
             test: /\.(js|jsx)$/,
             loaders: ['babel'],
             // exclude: /node_modules/,
             include: [__dirname, path.resolve(__dirname, './../src'), path.resolve(__dirname, './../dist')]
         }
         ]
-<<<<<<< HEAD
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
     ]
-=======
-    }
->>>>>>> master
 }
