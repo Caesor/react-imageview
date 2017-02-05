45a1990602a0975b050d348e72046e9a6936b99b
nemoliao(廖泽恺) <nemoliao@tencent.com>
2017-02-05 15:48:39 +0800

    fix bad image

--- examples/index.js
+++ examples/index.js
@@ -19,7 +19,7 @@ class Main extends Component {
             '//gpic.qpic.cn/gbar_pic/hVlQlSGMCtYlKrqpM5xwdmJrbh4iaawOgY6lFT1eNWTib7qv2Z2QuJWXmchPUqBriay/1000',
             '//gpic.qpic.cn/gbar_pic/lDVAjxOVicMnyU4OWLShicffM3TvZYFia4ywL0B5oC3BLPDCoIkgdkJLA/0',
             '//gpic.qpic.cn/gbar_pic/2aqluyraXicEfqicaK3aV4ia3YQE3mKcibH02jibympJ4gzCUEjk2Iz5BwQ/1000',
-            '//inews.gtimg.com/newsapp_match/0/968418161/0',
+            '//gpic.qpic.cn/gbar_pic/rqlh3lfegUYAvWGGNA8wyC5kly2PwLzONQsSatcxicqJOw0gz9MGmZg/1000',
             '//gpic.qpic.cn/gbar_pic/PR0vBBjLNC7PpwKQ5YmKjo9ricr8EqAZFQVzXJG96SKCr4hVoWiaT4OQ/0',
         ];
 
